2141b85b55399b2678ae35d9d58838f851fd1d41
Florian Koch <flkoch@dev.fkoch.ch>
2024-12-31 12:11:41 +0100

    Added configuration options for LoadingIndicator.

--- frontend/src/components/LoadinIndicator.tsx
+++ frontend/src/components/LoadinIndicator.tsx
@@ -1,8 +1,15 @@
 import { Spinner } from "react-bootstrap"
 
-const LoadingIndicator = () => {
+function LoadingIndicator({ size = "3rem", center = true }: { size?: string, center?: boolean }) {
+    if (center) return (
+        <div className="text-center" >
+            <Spinner animation="border" role="status" style={{ "width": size, "height": size }}>
+                <span className="visually-hidden">Loading...</span>
+            </Spinner>
+        </div>
+    )
     return (
-        <Spinner animation="border" role="status">
+        <Spinner animation="border" role="status" style={{ "width": size, "height": size }}>
             <span className="visually-hidden">Loading...</span>
         </Spinner>
     )
