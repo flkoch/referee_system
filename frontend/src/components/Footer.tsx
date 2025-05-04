395aedede83da692d989f1c504738151b9dbe890
Florian Koch <flkoch@dev.fkoch.ch>
2025-05-04 12:38:55 +0200

    Refined style

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,8 +1,8 @@
 export function Footer() {
     return (
-        <footer className="bg-secondary bg-gradient mt-5">
+        <footer className="bg-secondary bg-gradient text-dark mt-5">
             <div className="container py-sm-3 py-xl-4">
-                Copyright &copy; {new Date().getFullYear()} by <a href="mailto:florian.koch@sbeo.ch" className="link-dark">Florian Koch</a>
+                Copyright &copy; {new Date().getFullYear()} by <a href="mailto:florian.koch@sbeo.ch" className="link-dark link-underline-opacity-25 link-underline-opacity-100-hover">Florian Koch</a>
             </div>
         </footer>
     )
